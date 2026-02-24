b318ba64bc325504c0f949de9bbad8f2eb38de17
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-02-24 20:29:37 +0000

    parameter pass via URL

--- carsharing.py
+++ carsharing.py
@@ -1,11 +1,8 @@
-from datetime import datetime
-
 from fastapi import FastAPI
 
 app = FastAPI()
 
-
 @app.get("/")
-def welcome():
-    """Return a friendly welcome message."""
-    return {'message': "Welcome to the Car Sharing service!"}
+async def welcome(name):
+    """Return a welcome message."""
+    return {'message': f"Welcome, {name.upper()} to the Car Sharing service!"}
