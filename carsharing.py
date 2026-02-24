dcb26181ab37d78d2ac85eaf86e1216c1641280f
J Patrick McKeown <j.p.mckeown@icloud.com>
2026-02-24 21:04:44 +0000

    data and filter by car size category

--- carsharing.py
+++ carsharing.py
@@ -1,6 +1,25 @@
 from fastapi import FastAPI
 
 app = FastAPI()
+db = [
+    {"id": 1, "size": "s", "fuel": "gasoline", "doors": 3, "transmission": "auto"},
+    {"id": 2, "size": "s", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 3, "size": "s", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 4, "size": "m", "fuel": "electric", "doors": 3, "transmission": "auto"},
+    {"id": 5, "size": "m", "fuel": "hybrid", "doors": 5, "transmission": "auto"},
+    {"id": 6, "size": "m", "fuel": "gasoline", "doors": 5, "transmission": "manual"},
+    {"id": 7, "size": "l", "fuel": "diesel", "doors": 5, "transmission": "manual"},
+    {"id": 8, "size": "l", "fuel": "electric", "doors": 5, "transmission": "auto"},
+    {"id": 9, "size": "l", "fuel": "hybrid", "doors": 5, "transmission": "auto"}
+]
+
+@app.get("/cars/")
+async def get_cars(size=None):
+    """Retrieve all cars."""
+    if size:
+        return [x for x in db if x['size'] == size]
+    else:
+        return db
 
 @app.get("/")
 async def welcome(name):
